27326958f6ded4c0dc359b2fab9a951b6415c714
grigor1986 <grigtaneev@gmail.com>
2026-09-08 12:47:51 +0300

    Быстрый поиск по номеру паспорта, отредактирован

--- MyWork1/src/collections/QuickSearch.java
+++ MyWork1/src/collections/QuickSearch.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class QuickSearch {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
